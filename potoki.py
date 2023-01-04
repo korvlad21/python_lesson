897bbe0b3bc4aef750dcad3a7855f20f03578f86
Vlad <vladek2000@inbox.ru>
2023-01-04 16:19:54 +0400

    1:12:33

--- potoki.py
+++ potoki.py
@@ -1,15 +1,19 @@
 import os
 
-sayt = input()
+while True:
+    sayt = input('Введите адрес сайта\n')
 
-if 'https://' in sayt:
-    os.system('open ' + sayt)
-    print('if')
-elif 'www.' in sayt:
-    sayt = 'https://' + sayt
-    os.system('open ' + sayt)
-    print('elif')
-else:
-    sayt = 'https://www.' + sayt
-    os.system('open ' + sayt)
-    print('else')        
+    if sayt == 'завершить':
+        break
+
+    if 'https://' in sayt:
+        os.system('open ' + sayt)
+        print('if')
+    elif 'www.' in sayt:
+        sayt = 'https://' + sayt
+        os.system('open ' + sayt)
+        print('elif')
+    else:
+        sayt = 'https://www.' + sayt
+        os.system('open ' + sayt)
+        print('else')        
